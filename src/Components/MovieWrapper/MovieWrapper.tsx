e15f487a1697a6af55adb823b173bfe469c97bef
amirabazi <abaziamirr@gmail.com>
2021-12-08 00:30:22 +0100

    apis + modal

--- src/Components/MovieWrapper/MovieWrapper.tsx
+++ src/Components/MovieWrapper/MovieWrapper.tsx
@@ -1,25 +1,81 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Modal from 'react-modal';
 import './MovieWrapper.css'
-
+import { FiArrowLeft } from "react-icons/fi";
+import axios from 'axios'
 interface IProps {
+    id: number,
     image: string,
     overview: string,
     title: string,
-    vote_average: number
-
+    vote_average: number,
+    apiKey: string,
+}
+interface ITrailer {
+    trailer: {
+        id: string,
+        key: string,
+        name: string,
+    }[],
 }
 
-function MovieWrapper({ image, title, vote_average, overview }: IProps) {
+function MovieWrapper({ image, title, vote_average, overview, apiKey, id, }: IProps) {
+    const customStyles = {
+        content: {
+            top: '50%',
+            left: '50%',
+            right: 'auto',
+            bottom: 'auto',
+            marginRight: '-50%',
+            transform: 'translate(-50%, -50%)',
+        },
+    };
+    const [modalIsOpen, setModalIsOpen] = useState(false);
+    const [videoFetch, setVideoFetch] = useState<ITrailer["trailer"]>([])
+
+    function openModal(): void {
+        setModalIsOpen(true);
+        getVideo();
+    }
+
+    function closeModal(): void {
+        setModalIsOpen(false);
+    }
+
+    function getVideo(): void {
+        axios.get("https://api.themoviedb.org/3/movie/" + id + "/videos?api_key=" + apiKey + "&language=en-US")
+            .then((res) => {
+                setVideoFetch(res.data.results[0].key)
+            }).catch((error) => {
+            })
+    }
     return (
         <div className="moviewrapper">
-            <div className="img__wrapper">
+            <div className="img__wrapper" onClick={openModal}>
                 <img className="img" src={image} alt="movieImg" />
             </div>
             <div className="about__wrapper">
                 <p className="score">{vote_average}</p>
                 <h3 className="title">{title}</h3>
-                <p className="overview">{overview}</p>
             </div>
+            <Modal
+                isOpen={modalIsOpen}
+                style={customStyles}
+            >
+                <div className="modalDiv">
+                    <div className="backBtn" onClick={closeModal}>
+                        <FiArrowLeft className="icon" />
+                        <p >Back</p>
+                    </div>
+                    <div className="modalImg">
+                        <iframe width="100%" height="315"
+                            src={"https://www.youtube.com/embed/" + videoFetch}>
+                        </iframe>
+                    </div>
+                    <h2 className="modalTitle">{title}</h2>
+                    <p className="modalOverview">{overview}</p>
+                </div>
+            </Modal>
         </div>
     )
 }
--- src/Views/Homepage.tsx
+++ src/Views/Homepage.tsx
@@ -1,69 +1,80 @@
-import React, { useEffect, useState } from 'react'
-
+import _ from 'lodash';
+import React, { useEffect, useState, useCallback } from 'react'
 import './Homepage.css'
 import axios from 'axios'
 import MovieWrapper from '../Components/MovieWrapper/MovieWrapper';
-import TvShowsWrapper from '../Components/TvShowsWrapper/TvShowsWrapper';
+import { apiClient } from '../ApiClient';
 
-interface IApiCall {
-    movies: {
-        backdrop_path: string,
-        overview: string,
-        title: string,
-        vote_average: number
-
-    }[],
-    series: {
-        backdrop_path: string,
-        overview: string,
-        name: string,
-        vote_average: number
-    }[],
-    searchedMovies: {
-        backdrop_path: string,
-        original_title: string,
-        overview: string,
-        vote_average: number
-    }[],
-
+type MovieAndSeriesData = {
+    id: number;
+    backdrop_path: string;
+    overview: string;
+    title: string;
+    vote_average: number;
 }
-
+interface IHooks {
+    onClick: () => void;
+}
 
 function Homepage() {
     const apiKey = "1a049c4595923c5bcca0118888ab8aea";
-    const [movies, setMovies] = useState<IApiCall["movies"]>([]);
-    const [series, setSeries] = useState<IApiCall["series"]>([]);
-    const [searchedMovies, setSearchedMovies] = useState<IApiCall["searchedMovies"]>([]);
+    const [movies, setMovies] = useState<MovieAndSeriesData[]>([]);
+    const [series, setSeries] = useState<MovieAndSeriesData[]>([]);
+    const [searchedMovies, setSearchedMovies] = useState<MovieAndSeriesData[]>([]);
     const [movieToggle, setMovieToggle] = useState(false);
     const [search, setSearch] = useState("");
     const [seriesToggle, setSeriesToggle] = useState(false);
+    const debounceSearch = useCallback(_.debounce(doSearch, 3000), []);
 
+    function doSearch(inputvalue: string): void {
+        if (inputvalue.length < 3) {
+            return;
+        }
+
+        axios.get("https://api.themoviedb.org/3/search/movie?api_key=" + apiKey + "&language=en-US&query=" + inputvalue + "&page=1&include_adult=false")
+            .then((res) => {
+                setSearchedMovies(res.data.results);
+                console.log('search api', searchedMovies)
+            }).catch((error) => {
+                console.log(error);
+            })
+        console.log('inputVal', inputvalue)
+    }
+
+    function searchEvent(event: any): void {
+        setSearch(event.target.value);
+        debounceSearch(event.target.value);
+    }
 
     useEffect(() => {
         axios.get("https://api.themoviedb.org/3/movie/top_rated?api_key=" + apiKey + "&language=en-US&page=1")
             .then((response) => {
                 setMovies(response.data.results)
+                console.log(movies)
             }).catch((error) => {
                 console.log(error);
             })
 
         axios.get("https://api.themoviedb.org/3/tv/top_rated?api_key=" + apiKey + "&language=en-US&page=1")
             .then((result) => {
-                setSeries(result.data.results)
+                if (result.data.results !== undefined) {
+                    const series: MovieAndSeriesData[] = result.data.results.map((res: any) => {
+                        return {
+                            id: res.id,
+                            backdrop_path: res.backdrop_path,
+                            overview: res.title,
+                            title: res.name,
+                            vote_average: res.vote_average,
+                        }
+                    });
+                    setSeries(series);
+                }
             }).catch((error) => {
                 console.log(error);
             })
-    }, [movieToggle, seriesToggle])
+    }, [])
 
-    useEffect(() => {
-        axios.get("https://api.themoviedb.org/3/search/movie?api_key=" + apiKey + "&language=en-US&query=" + search + "&page=1&include_adult=false")
-            .then((res) => {
-                setSearchedMovies(res.data.results);
-            }).catch((error) => {
-                console.log(error);
-            })
-    }, [search])
-    console.log(searchedMovies)
+
 
     return (
         <div className="homepage">
@@ -86,34 +97,44 @@ function Homepage() {
                     <input type="text"
                         className="search"
                         placeholder="search a movie..."
-                        onChange={(e) => {
-                            setSearch(e.target.value)
-                        }} />
+                        onChange={searchEvent} />
                 </form>
             </div>
             <div className="homepage__content">
                 {movieToggle ?
                     <div className="movies__page">
-
                         {
                             movies.slice(0, 10).map((movie, index) => {
                                 return (
-                                    <MovieWrapper key={index} vote_average={movie.vote_average} title={movie.title} image={"https://image.tmdb.org/t/p/original" + movie.backdrop_path} overview={movie.overview} />
+                                    <MovieWrapper key={index}
+                                        id={movie.id}
+                                        vote_average={movie.vote_average}
+                                        title={movie.title as string}
+                                        image={"https://image.tmdb.org/t/p/original" + movie.backdrop_path}
+                                        overview={movie.overview}
+                                        apiKey={apiKey}
+                                    />
                                 )
                             })
                         }
                     </div>
-                    : <div className="tvshows__page">
+                    :  /* <- or */
+                    <div className="tvshows__page">
                         {
                             series.slice(0, 10).map((series, index) => {
                                 return (
-                                    <TvShowsWrapper key={index} vote_average={series.vote_average} name={series.name} image={"https://image.tmdb.org/t/p/original" + series.backdrop_path} title={series.overview || "TBA"} />
+                                    <MovieWrapper key={index}
+                                        id={series.id}
+                                        vote_average={series.vote_average}
+                                        title={series.title as string}
+                                        apiKey={apiKey}
+                                        image={"https://image.tmdb.org/t/p/original" + series.backdrop_path}
+                                        overview={series.overview} />
                                 )
                             })
                         }
                     </div>
                 }
-
             </div>
         </div>
     )
